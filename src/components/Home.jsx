2ac125eb88746dc48c5256ef9b17e14df5b855a3
Disha <rupaparadisha@gmail.com>
2024-12-24 14:48:35 -0500

    new added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import pdf from "../pdf/Resume.pdf";
+import pdf from "../pdf/Disha_Resume.pdf";
 import hero from "./data/hero.json";
 import Typed from "typed.js";
 
@@ -33,7 +33,7 @@ const Home = () => {
 
           <a
             href={pdf}
-            download="Resume.pdf"
+            download="Disha_Resume.pdf"
             className="btn btn-outline-warning my-3"
           >
             Download Resume
